342b81da1ff8ac1e64baa6b672d59d6745ed472b
DingerBtn <DingerBtn@aliyun.com>
2025-07-01 02:28:58 +0800

    首次提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Untitled 服务器文档站',
+            // https://starlight.astro.build/zh-cn/guides/customization/#%E7%A6%81%E7%94%A8%E9%BB%98%E8%AE%A4-404-%E9%A1%B5%E9%9D%A2
+            disable404Route: true,
+            logo: {
+                src: './src/assets/logo_64_64.svg',
+            },
+            // https://starlight.astro.build/zh-cn/reference/configuration/#favicon
+            favicon: '/logo_64_64.ico',
+            // https://starlight.astro.build/zh-cn/reference/configuration/#lastupdated
+            lastUpdated: true,
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/DingerBtn/UntitledServerRule'}],
+            sidebar: [
+                {
+                    label: '规则（详细版）',
+                    autogenerate: {directory: '规则（详细版）'},
+                },
+                {
+                    label: '公示及教程',
+                    autogenerate: {directory: '公示及教程'},
+                },
+            ],
+            // https://starlight.astro.build/zh-cn/reference/configuration/#lastupdated
+            locales: {
+                root: {
+                    label: '简体中文',
+                    lang: 'zh-CN',
+                },
+            },
+        }),
+    ],
 });
